159a47aeff8244882482d0dced26ebd28ca647d6
Randhir200 <mail.randhirkr@gmail.com>
2022-08-11 11:05:01 +0530

    more node added

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-class Node{
-    constructor(data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-let newNode = new Node(4)
-console.log(newNode)
+let root = new Node(4);
+let node2 = new Node(6);
+let node3 = new Node(7);
+let node4 = new Node(8);
+let node5 = new Node(9);
+let node6 = new Node(10);
+root.left = node2;
+root.right = node3;
+node2.left = node4;
+node2.right = node5;
+node5.left = node6;
+
+                 //       node
+                 //      /    \
+                 //    node    node
+                 //   /   \
+                 // node   node
+                 //         \
+                 //          node
+console.log(root);
